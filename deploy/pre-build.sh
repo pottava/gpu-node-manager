e66f0d6fc957f791819b20c9727b5fa5fa389985
Ryo Nakamaru <pottava@gmail.com>
2022-12-14 13:54:25 +0900

    revise pipelines

--- deploy/pre-build.sh
+++ deploy/pre-build.sh
@@ -2,6 +2,6 @@
 
 echo 'target/\ntest-results/\ntmp/\nroutes/\nkey.json' > src/.gitignore
 
-url=$( gcloud run services describe dev --region "$1" \
+url=$( gcloud run services describe "$1" --region "$2" \
     --format 'value(status.address.url)' )
 echo "window.apiBaseURL = \"${url}\";" > src/public/js/app/config.js
